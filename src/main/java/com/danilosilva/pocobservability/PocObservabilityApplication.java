5b5c615e53374c01056323cb9b36941f314bc735
Danilo Silva <danilosilvap@outlook.com.br>
2022-06-19 14:19:01 -0300

    feat: create project

---
+++ src/main/java/com/danilosilva/pocobservability/PocObservabilityApplication.java
@@ -0,0 +1,13 @@
+package com.danilosilva.pocobservability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocObservabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocObservabilityApplication.class, args);
+	}
+
+}
