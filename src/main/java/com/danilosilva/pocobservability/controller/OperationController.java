3f9ecadf75d2eb3c04687d8a5fbcf2e35d001525
Danilo Silva <danilosilvap@outlook.com.br>
2022-07-07 15:39:12 -0300

    feat: add custom metrics

--- src/main/java/com/danilosilva/pocobservability/controller/OperationController.java
+++ src/main/java/com/danilosilva/pocobservability/controller/OperationController.java
@@ -2,6 +2,9 @@ package com.danilosilva.pocobservability.controller;
 
 import static com.danilosilva.pocobservability.support.Constants.DOCUMENT_IDENTIFIER;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +13,20 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @Slf4j
 public class OperationController {
+
+    private final MeterRegistry meterRegistry;
+    private final Counter.Builder exclusiveRequests = Counter.builder("exclusive_request")
+        .description("The number of requests in created endpoints");
 
     @GetMapping(value = "success")
     public ResponseEntity<String> getSuccess(@RequestHeader("document-identifier") final String documentIdentifier) {
         MDC.put(DOCUMENT_IDENTIFIER, documentIdentifier);
         log.info("GET Request to success endpoint");
+
+        this.incrementExclusiveRequest("success");
         return ResponseEntity.ok("Yep, success!");
     }
 
@@ -26,6 +36,16 @@ public class OperationController {
     ) throws IllegalAccessException {
         MDC.put(DOCUMENT_IDENTIFIER, documentIdentifier);
         log.info("GET Request to fails endpoint");
+
+        this.incrementExclusiveRequest("failed");
         throw new IllegalAccessException("Ops, fails...");
     }
+
+    private void incrementExclusiveRequest(final String type) {
+        exclusiveRequests
+            .tag("type", type)
+            .register(meterRegistry)
+            .increment();
+    }
+
 }
